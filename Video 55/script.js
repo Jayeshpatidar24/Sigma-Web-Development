58af0c9a8cc50dc8047ef96b23da40879648ea4f
Jayesh Patidar <92778917+Jayeshpatidar24@users.noreply.github.com>
2025-03-11 15:08:15 +0530

    Video 55

---
+++ Video 55/script.js
@@ -0,0 +1,27 @@
+console.log("Hey i just start my javascript")
+
+// var a=5;
+// var c = "name";
+// var b=3;
+// console.log(a+b+4 +c)
+
+// console.log(typeof(a),typeof(b),typeof(c)) 
+
+let obj = {
+    name : "Jayesh",
+    "Age" : 20 ,
+    "is Male" : true 
+}
+
+console.log(obj)
+
+console.log(typeof(obj) )
+
+let a = undefined;
+let b = null;
+let c = "name";
+let d =5;
+let e = true;
+let f = {};
+
+console.log(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(f))
