b4a352388927ed43cabdedcccaab7c993615708f
Jayesh Patidar <92778917+Jayeshpatidar24@users.noreply.github.com>
2025-04-18 23:50:07 +0530

    Video 56

---
+++ Video 56/script.js
@@ -0,0 +1,49 @@
+// console.log("hey i am ready to use java script in terminal")
+
+
+// console.log("lets make one program ")
+
+// let age =  2;
+
+// if(age > 18){
+//     console.log("You can drive");
+
+// }else {
+//         console.log("you can not drive");
+
+//     }
+
+
+//     console.log(2+2);
+//     console.log(2-2);   
+//     console.log(2*2);
+//     console.log(2/2);
+//     console.log(2%2);
+//     console.log(2**2);
+//     console.log(2+2*2);
+//     console.log((2+2)*2);
+
+
+//     let obj1 = {
+//         name : "sachin",
+//     }
+
+   
+      
+//         console.log(obj1.name);
+
+
+        let num1 = 12;
+        let num2 = 34;
+        console.log(num1);
+        console.log(num2);
+        let num3 = (num1>num2 ? num1+num2 : num1 = num2 + 10);
+        console.log(num3);
+        console.log(num1);
+        console.log(num2);
+
+        console.log("hello world");
+
+        for(let i = 0; i < 5; i++){
+            console.log(i);
+        }
