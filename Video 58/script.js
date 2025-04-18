9b06f228ac403108e9c1939698dcb89fe4b60637
Jayesh Patidar <92778917+Jayeshpatidar24@users.noreply.github.com>
2025-04-18 23:50:31 +0530

    Video 58

---
+++ Video 58/script.js
@@ -0,0 +1,32 @@
+console.log("lets start functions")
+
+
+function letFunction(a , b){
+console.log("sum of the : "+ (a+b))
+}
+
+
+letFunction(1 ,2)
+
+
+
+const name = ( x ) => {
+    console.log("hey fuction pata nhi kya hai " + "Jo bhi value hai vo aayegi in the place of x : " + x)
+}
+
+let b = name(33)
+ 
+
+function printingNmae(name){
+    console.log("hey i'm "+name+" nice to meet you!")
+}
+
+printingNmae("jayesh")
+
+function calculator(a ,b ){
+    return a+b;
+}
+
+
+console.log(calculator(5,6)
+)
