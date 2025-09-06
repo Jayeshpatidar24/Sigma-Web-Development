92377e452e915567ce2a1ece690cf3efaa2ed7d2
Jayesh Patidar <92778917+Jayeshpatidar24@users.noreply.github.com>
2025-09-06 14:28:00 +0530

    Video 55

--- Video 55/script.js
+++ Video 55/script.js
@@ -1,27 +1,74 @@
-console.log("Hey i just start my javascript")
+// console.log("Hey i just start my javascript")
 
-// var a=5;
-// var c = "name";
-// var b=3;
-// console.log(a+b+4 +c)
+// // var a=5;
+// // var c = "name";
+// // var b=3;
+// // console.log(a+b+4 +c)
 
-// console.log(typeof(a),typeof(b),typeof(c)) 
+// // console.log(typeof(a),typeof(b),typeof(c)) 
 
-let obj = {
-    name : "Jayesh",
-    "Age" : 20 ,
-    "is Male" : true 
+// let obj = {
+//     name : "Jayesh",
+//     "Age" : 20 ,
+//     "is Male" : true 
+// }
+
+// console.log(obj)
+
+// console.log(typeof(obj) )
+
+// let a = undefined;
+// let b = null;
+// let c = "name";
+// let d =5;
+// let e = true;
+// let f = {};
+
+// console.log(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(f))
+
+// var name1 = "Jayesh";
+// console.log(name1+" is a good")
+
+// let o = {
+//     "name" : "Jayesh",
+//     "age" : 20,
+//     "isMale" : true,
+//     "powerLifter" : "yo"
+// }
+// typeof(o) ==  "object" ? console.log("y") : console.log("n")
+// console.log(o.isMale)
+// console.log(o.powerLifter)
+
+// let addName = "Jayesh";
+// console.log("Hello " + addName + 25)
+// console.log(typeof(addName))
+
+
+// typeof(addName) == "string" ? console.log("yes") : console.log("no")
+
+// let myFreeze = {name : "Jayesh", age : 25};
+//  myFreeze.age = 30
+// console.log(myFreeze.age)
+
+
+//dictionary 
+
+//  let firstDictionary  = {
+//     age : 20,
+//     name : "Jayesh",    
+//  }
+//  firstDictionary.age = 21
+//  firstDictionary["name"] = "jay"
+
+//     console.log(firstDictionary.age)
+//     console.log(firstDictionary["name"]);
+
+let idStu =  {
+   name : "Jayesh",
+   age : 20,
+   is_handsome : true
+
 }
 
-console.log(obj)
-
-console.log(typeof(obj) )
-
-let a = undefined;
-let b = null;
-let c = "name";
-let d =5;
-let e = true;
-let f = {};
-
-console.log(typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(f))
+console.log(idStu.age , idStu.is_handsome, idStu.name)
+console.log(idStu.name);
